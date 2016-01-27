960736b4eaff676c3a260b9d04a865e782fe5a7b
Abigail Watson <awatson1978@gmail.com>
2016-01-26 20:57:46 -0800

    initial commit

---
+++ client/mainPage/mainPage.js
@@ -0,0 +1,28 @@
+
+Router.route('/', {
+  template: "mainPage",
+  name: "mainPage",
+  onAfterAction: function(){
+    Template.appLayout.delayedLayout(10);
+    Template.appLayout.delayedLayout(100);
+    Template.appLayout.delayedLayout(1000);
+  }
+});
+
+
+Template.mainPage.rendered = function(){
+    Template.appLayout.delayedLayout(10);
+    Template.appLayout.delayedLayout(100);
+    Template.appLayout.delayedLayout(1000);
+};
+
+
+Template.mainPage.events({
+  'click #foalRegistration':function(){
+    Router.go('/grid/foos');
+  }
+});
+
+Template.mainPage.helpers({
+
+});
