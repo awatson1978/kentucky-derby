29cf7d076c91887c3953e7ebe1b35e3362cfef27
Abigail Watson <awatson1978@gmail.com>
2016-05-12 14:54:00 -0500

    derby142

---
+++ derby142.js
@@ -0,0 +1,420 @@
+12th Race - Churchill Downs - Saturday, May 7th, 2016
+
+Group = {
+  type: "animal",
+  name: "Kentucky Derby 142, Race 12",
+  quantity: 22,
+  description: "STAKES. 1 1/4 Mile Dirt. Purse $2,000,000. Kentucky Derby Presented by Yum! Brands (Grade 1). FOR THREE-YEAR-OLDS. (06:34 PM) (22)",
+  member: [{
+    display: "Trojan Nation",
+    reference: null
+  }, {
+    display: "Suddenbreakingnews",
+    reference: null
+  },  {
+    display: "Creator",
+    reference: null
+  },  {
+    display: "Mo Tom",
+    reference: null
+  },  {
+    display: "Gun Runner",
+    reference: null
+  },  {
+    display: "My Man Sam",
+    reference: null
+  },  {
+    display: "Oscar Nominated",
+    reference: null
+  },  {
+    display: "Lani",
+    reference: null
+  },  {
+    display: "Destin",
+    reference: null
+  },  {
+    display: "Whitmore",
+    reference: null
+  },  {
+    display: "Exaggerator",
+    reference: null
+  },  {
+    display: "Tom's Ready",
+    reference: null
+  },  {
+    display: "Nyquist",
+    reference: null
+  },  {
+    display: "Mohaymen",
+    reference: null
+  },  {
+    display: "Outwork",
+    reference: null
+  },  {
+    display: "Shagaf",
+    reference: null
+  },  {
+    display: "Mor Spirit",
+    reference: null
+  },  {
+    display: "Majesto",
+    reference: null
+  },  {
+    display: "Brody's Cause",
+    reference: null
+  },  {
+    display: "Danzing Candy",
+    reference: null
+  },  {
+    display: "Laoban",
+    reference: null
+  },  {
+    display: "Cherry Wine",
+    reference: null
+  }]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+{
+  number: 1,
+  horse: "Trojan Nation",
+  state: "KY",
+  odds: "50",
+  jockey: "Aaron T. Gryder",
+  weight: "126",
+  age: 3"",
+  sex: "C",
+  medications: "L",
+  equipment: "",
+  trainer: "Patrick Gallagher",
+  owners: "Gilbert, Julie and Sones, Aaron",
+  breeders: "Dr. Aaron Sones"
+},  {
+  number: 2,
+  horse: "Suddenbreakingnews",
+  state: "KY",
+  odds: "20",
+  jockey: "Luis S. Quinonez",
+  weight: "126",
+  age: "3",
+  sex: "G",
+  medications: "L",
+  equipment: "",
+  trainer: "Donnie K. Von Hemel",
+  owners: "Samuel F. Henderson",
+  breeders: "Branch Equine, LLC"
+},  {
+  number: 3,
+  horse: "Creator",
+  state: "KY",
+  odds: "10",
+  jockey: "Ricardo Santana, Jr.",
+  weight: "126",
+  age: "3",
+  sex: "C",
+  medications: "L",
+  equipment: "",
+  trainer: "Steven M. Asmussen",
+  owners: "WinStar Farm LLC",
+  breeders: "Mt.Brilliant Broodmares I LLC"
+},  {
+  number: 4,
+  horse: "Mo Tom",
+  state: "KY",
+  odds: "20",
+  jockey: "Corey J. Lanerie",
+  weight: "126",
+  age: "3",
+  sex: C"",
+  medications: "L",
+  equipment: "",
+  trainer: "Thomas M. Amoss",
+  owners: "G M B Racing",
+  breeders: "Hargus & Sandra Sexton & Silver FernFarm"
+},  {
+  number: 5,
+  horse: "Gun Runner",
+  state: "KY",
+  odds: "10",
+  jockey: "Florent Geroux",
+  weight: "126",
+  age: "3",
+  sex: "C",
+  medications: "L",
+  equipment: "",
+  trainer: "Steven M. Asmussen",
+  owners: "Winchell Thoroughbreds LLC, Three Chimneys Farm, LLC and Besilu Stables",
+  breeders: "Besilu Stables, LLC"
+},  {
+  number: 6,
+  horse: "My Man Sam",
+  state: "KY",
+  odds: "20",
+  jockey: "Irad Ortiz, Jr.",
+  weight: "126",
+  age: "3",
+  sex: "C",
+  medications: "L",
+  equipment: "",
+  trainer: "Chad C. Brown",
+  owners: "Sheep Pond Partners, Newport Stables LLC and Bligh, Jay W.",
+  breeders: "Jay W. Bligh"
+},  {
+  number: 7,
+  horse: "Oscar Nominated",
+  state: "KY",
+  odds: "50",
+  jockey: "Julien R. Leparoux",
+  weight: "126",
+  age: "3",
+  sex: "C",
+  medications: "L",
+  equipment: "",
+  trainer: "Michael J. Maker",
+  owners: "Ramsey, Kenneth L. and Sarah K.",
+  breeders: "Mrs. Jerry Amerman"
+},  {
+  number: 8,
+  horse: "Lani",
+  state: "KY",
+  odds: "30",
+  jockey: "Yutaka Take",
+  weight: "126",
+  age: "3",
+  sex: "C",
+  medications: "",
+  equipment: "",
+  trainer: "Mikio Matsunaga",
+  owners: "Koji Maeda",
+  breeders: "North Hills Co. Limited"
+},  {
+  number: 9,
+  horse: "Destin",
+  state: "KY",
+  odds: "15",
+  jockey: "Javier Castellano	",
+  weight: "126",
+  age: "3",
+  sex: "C",
+  medications: "L",
+  equipment: "",
+  trainer: "Todd A. Pletcher",
+  owners: "Twin Creeks Racing Stables, LLC and Eclipse Thoroughbred Partners",
+  breeders: "James C. Weigel &Taylor Made Stallions Inc."
+},  {
+  number: 10,
+  horse: "Whitmore",
+  state: "KY",
+  odds: "20",
+  jockey: "Victor Espinoza",
+  weight: "126",
+  age: "3",
+  sex: "C",
+  medications: "L",
+  equipment: "",
+  trainer: "Ron Moquett",
+  owners: "LaPenta, Robert V., Rosenblum, Harry T. and Southern Springs Stables",
+  breeders: "John Liviakis"
+},  {
+  number: 11,
+  horse: "Exaggerator",
+  state: "KY",
+  odds: "8",
+  jockey: "Kent J. Desormeaux",
+  weight: "126",
+  age: "3",
+  sex: "C",
+  medications: "L",
+  equipment: "",
+  trainer: "Keith Desormeaux",
+  owners: "Big Chief Racing, LLC, Head of Plains Partners LLC, Rocker O Ranch, LLC et al.",
+  breeders: "Joseph B. Murphy"
+},  {
+  number: 12,
+  horse: "Tom's Ready",
+  state: "PA",
+  odds: "30",
+  jockey: "Brian Joseph Hernandez, Jr.",
+  weight: "126",
+  age: "3",
+  sex: "C",
+  medications: "L",
+  equipment: "",
+  trainer: "Dallas Stewart",
+  owners: "G M B Racing",
+  breeders: "Blackstone Farm LLC"
+},  {
+  number: 13
+  horse: "Nyquist",
+  state: "KY",
+  odds: "3",
+  jockey: "Mario Gutierrez",
+  weight: "126",
+  age: "3",
+  sex: "C",
+  medications: "L",
+  equipment: "",
+  trainer: "Doug F. O'Neill",
+  owners: "Reddam Racing LLC",
+  breeders: "Summerhill Farm"
+},  {
+  number: 14,
+  horse: "Mohaymen",
+  state: "KY",
+  odds: "10",
+  jockey: "Junior Alvarado",
+  weight: "126",
+  age: "3",
+  sex: "C",
+  medications: "L",
+  equipment: "",
+  trainer: "Kiaran P. McLaughlin",
+  owners: "Shadwell Stable",
+  breeders: "Clearsky Farms"
+},  {
+  number: 15
+  horse: "Outwork",
+  state: "KY",
+  odds: "15",
+  jockey: "John R. Velazquez",
+  weight: "126",
+  age: "3",
+  sex: "C",
+  medications: "L",
+  equipment: "",
+  trainer: "Todd A. Pletcher",
+  owners: "Repole Stable",
+  breeders: "Repole Stable, Inc"
+},  {
+  number: 16,
+  horse: "Shagaf",
+  state: "KY",
+  odds: "20",
+  jockey: "Joel Rosario",
+  weight: "126",
+  age: "3",
+  sex: "C",
+  medications: "L",
+  equipment: "",
+  trainer: "Chad C. Brown",
+  owners: "Shadwell Stable",
+  breeders: "Shadwell Farm, LLC"
+},  {
+  number: 17,
+  horse: "Mor Spirit",
+  state: "PA",
+  odds: "12",
+  jockey: "Gary L. Stevens",
+  weight: "126",
+  age: "3",
+  sex: "R",
+  medications: "L",
+  equipment: "",
+  trainer: "Bob Baffert",
+  owners: "Michael Lund Petersen",
+  breeders: "Elkstone Group, LLC"
+},  {
+  number: 18,
+  horse: "Majesto",
+  state: "KY",
+  odds: "30",
+  jockey: "Emisael Jaramillo	",
+  weight: "126",
+  age: "3",
+  sex: "R",
+  medications: "L",
+  equipment: "",
+  trainer: "Gustavo Delgado",
+  owners: "Grupo 7C Racing Stable",
+  breeders: "Diamond A Racing Corp."
+},  {
+  number: 19,
+  horse: "Brody's Cause",
+  state: "KY",
+  odds: "12",
+  jockey: "Luis Saez",
+  weight: "126",
+  age: "3",
+  sex: "C",
+  medications: "L",
+  equipment: "",
+  trainer: "Dale L. Romans",
+  owners: "Albaugh Family Stable",
+  breeders: "Gabriel Duignan, William Arvin, Jr. &Petaluma Bloodstock"
+},  {
+  number: 20,
+  horse: "Danzing Candy",
+  state: "KY",
+  odds: "15",
+  jockey: "Mike E. Smith",
+  weight: "126",
+  age: "3",
+  sex: "C",
+  medications: "L",
+  equipment: "",
+  trainer: "Clifford W. Sise, Jr.",
+  owners: "Halo Farms, Bashor, Jim and Bashor, Dianne",
+  breeders: "Halo Farms"
+},  {
+  number: 21,
+  horse: "Laoban",
+  state: "KY",
+  odds: "50",
+  jockey: "Cornelio H. Velasquez",
+  weight: "126",
+  age: "3",
+  sex: "C",
+  medications: "L",
+  equipment: "o",
+  trainer: "Eric J. Guillot",
+  owners: "McCormick Racing LLC and Southern Equine Stable LLC",
+  breeders: "Respite Farm"
+},  {
+  number: 22,
+  horse: "Cherry Wine",
+  state: "KY",
+  odds: "30",
+  jockey: "Robby Albarado",
+  weight: "126",
+  age: "3",
+  sex: "C",
+  medications: "L",
+  equipment: "",
+  trainer: "Dale L. Romans",
+  owners: "Pacella, William and Jones, Jr., Frank L.",
+  breeders: "Pacella Racing, Frank L Jones, Jr ,Frank Shoop"
+}
+
+
+
+raceEntries.forEach(function(entry){
+  // new patient
+  Patients.insert();
+  
+  // jockey
+  RelatedPersons.insert();
+  
+  // owner
+  RelatedPersons.insert();
+  
+  Location.insert();
+  
+  // trainer
+  Practitioners.insert();
+  
+  Organizations.insert();
+});
+
+// Lasix
+Medications.insert();
